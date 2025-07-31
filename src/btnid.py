6c00790c4baf2f7e26a2121d760ef67ebf2d80d9
Audionut <audionut11@gmail.com>
2025-07-31 13:40:28 +1000

    cleanup btn/bhd prints

--- src/btnid.py
+++ src/btnid.py
@@ -1,6 +1,7 @@
 import httpx
 import uuid
 from src.bbcode import BBCODE
+from src.console import console
 
 
 async def generate_guid():
@@ -46,15 +47,16 @@ async def get_btn_torrents(btn_api, btn_id, meta):
 
             if imdb_id and imdb_id != "0" and not meta.get("imdb_id"):
                 meta["imdb_id"] = int(imdb_id)
-                print("BTN IMDb ID:", meta["imdb_id"])
 
             if tvdb_id and tvdb_id != "0" and not meta.get("tvdb_id"):
                 meta["tvdb_id"] = int(tvdb_id)
-                print("BTN TVDb ID:", meta["tvdb_id"])
+
+            if meta.get("imdb_id") or meta.get("tvdb_id"):
+                console.print(f"[green]Found BTN IDs: IMDb={meta.get('imdb_id')}, TVDb={meta.get('tvdb_id')}")
 
             return meta
-
-    print("No IMDb or TVDb ID found.")
+    if meta['debug']:
+        console.print("[red]No IMDb or TVDb ID found.")
     return meta
 
 
@@ -178,9 +180,7 @@ async def get_bhd_torrents(bhd_api, bhd_rss_key, meta, only_id=False, info_hash=
         meta["description"] = ""
         meta["image_list"] = imagelist
 
-    if meta['debug']:
-        print("BHD IMDb ID:", meta.get("imdb_id"))
-        print("BHD TMDb ID:", meta.get("tmdb_id"))
+    console.print(f"[green]Found BHD IDs: IMDb={meta.get('imdb_id')}, TMDb={meta.get('tmdb_id')}")
 
     return meta["imdb_id"] or meta["tmdb_id"] or 0
 
