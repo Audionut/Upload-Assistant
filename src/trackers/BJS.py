042e10fcaf0ba90a214876dc328aa621e5d90e81
wastaken7 <wellytonsn@gmail.com>
2025-08-09 11:36:28 -0300

    update

--- src/trackers/BJS.py
+++ src/trackers/BJS.py
@@ -42,6 +42,7 @@ class BJS(COMMON):
         self.season = meta.get('season_int', '')
         self.episode = meta.get('episode_int', '')
         self.is_tv_pack = meta.get('tv_pack', '') == 1
+        self.cover = ''
 
     async def ptbr_tmdb_data(self, meta):
         self.assign_media_properties(meta)
@@ -128,7 +129,6 @@ class BJS(COMMON):
             return "Outro"
 
     async def search_existing(self, meta, disctype):
-        self.cover = ''
         if meta.get('debug'):
             return []
 
@@ -870,7 +870,7 @@ class BJS(COMMON):
             else:
                 if not self.cover:  # Only ask for input if there's no info in the site already
                     role_display_name = prompt_name_map.get(role, role.capitalize())
-                    prompt_message = f"-> {role_display_name} não encontrado(s). Por favor, insira manualmente (separados por vírgula): "
+                    prompt_message = (f"{role_display_name} não encontrado(s).\nPor favor, insira manualmente (separados por vírgula): ")
                     user_input = input(prompt_message)
 
                     if user_input.strip():
