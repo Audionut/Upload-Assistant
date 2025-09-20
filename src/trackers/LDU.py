434ab209cd6e30a74ad41c6ab75f67188485e6dc
Audionut <audionut11@gmail.com>
2025-09-20 16:06:21 +1000

    LDU

--- src/trackers/LDU.py
+++ src/trackers/LDU.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 # import discord
-import asyncio
-import aiofiles
-import platform
-import httpx
-import glob
-import os
 import langcodes
+from src.languages import has_english_language
 from src.trackers.COMMON import COMMON
 from src.console import console
-from src.languages import has_english_language
+from src.trackers.UNIT3D import UNIT3D
 
 
-class LDU():
+class LDU(UNIT3D):
     def __init__(self, config):
+        super().__init__(config, tracker_name='LDU')
         self.config = config
+        self.common = COMMON(config)
         self.tracker = 'LDU'
         self.source_flag = 'LDU'
-        self.upload_url = 'https://theldu.to/api/torrents/upload'
-        self.search_url = 'https://theldu.to/api/torrents/filter'
-        self.torrent_url = 'https://theldu.to/torrents/'
-        self.signature = "\n[center][url=https://github.com/Audionut/Upload-Assistant]Created by Audionut's Upload Assistant[/url][/center]"
+        self.base_url = 'https://theldu.to'
+        self.id_url = f'{self.base_url}/api/torrents/'
+        self.upload_url = f'{self.base_url}/api/torrents/upload'
+        self.search_url = f'{self.base_url}/api/torrents/filter'
+        self.torrent_url = f'{self.base_url}/torrents/'
         self.banned_groups = []
         pass
 
@@ -97,127 +95,6 @@ class LDU():
             type_id = '16'
         return type_id
 
-    async def get_res_id(self, resolution):
-        resolution_id = {
-            '8640p': '10',
-            '4320p': '1',
-            '2160p': '2',
-            '1440p': '3',
-            '1080p': '3',
-            '1080i': '4',
-            '720p': '5',
-            '576p': '6',
-            '576i': '7',
-            '480p': '8',
-            '480i': '9'
-        }.get(resolution, '10')
-        return resolution_id
-
-    async def upload(self, meta, disctype):
-        common = COMMON(config=self.config)
-        await common.edit_torrent(meta, self.tracker, self.source_flag)
-        cat_id = await self.get_cat_id(meta)
-        name = await self.edit_name(meta, cat_id)
-        type_id = await self.get_type_id(meta['type'], meta)
-        resolution_id = await self.get_res_id(meta['resolution'])
-        await common.unit3d_edit_desc(meta, self.tracker, self.signature)
-        region_id = await common.unit3d_region_ids(meta.get('region'))
-        distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
-        if meta['anon'] == 0 and not self.config['TRACKERS'][self.tracker].get('anon', False):
-            anon = 0
-        else:
-            anon = 1
-
-        if meta['bdinfo'] is not None:
-            mi_dump = None
-            async with aiofiles.open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8') as f:
-                bd_dump = await f.read()
-        else:
-            async with aiofiles.open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r', encoding='utf-8') as f:
-                mi_dump = await f.read()
-            bd_dump = None
-
-        async with aiofiles.open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]DESCRIPTION.txt", 'r', encoding='utf-8') as f:
-            desc = await f.read()
-        torrent_file_path = f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}].torrent"
-        async with aiofiles.open(torrent_file_path, 'rb') as f:
-            torrent_bytes = await f.read()
-        files = {'torrent': ('torrent.torrent', torrent_bytes, 'application/x-bittorrent')}
-        base_dir = meta['base_dir']
-        uuid = meta['uuid']
-        specified_dir_path = os.path.join(base_dir, "tmp", uuid, "*.nfo")
-        nfo_files = glob.glob(specified_dir_path)
-        if nfo_files:
-            async with aiofiles.open(nfo_files[0], 'rb') as f:
-                nfo_bytes = await f.read()
-            files['nfo'] = ("nfo_file.nfo", nfo_bytes, "text/plain")
-        data = {
-            'name': name,
-            'description': desc,
-            'mediainfo': mi_dump,
-            'bdinfo': bd_dump,
-            'category_id': cat_id,
-            'type_id': type_id,
-            'resolution_id': resolution_id,
-            'tmdb': meta['tmdb'],
-            'imdb': meta['imdb_id'],
-            'tvdb': meta['tvdb_id'],
-            'mal': meta['mal_id'],
-            'igdb': 0,
-            'anonymous': anon,
-            'stream': meta['stream'],
-            'sd': meta['sd'],
-            'keywords': meta['keywords'],
-            'personal_release': int(meta.get('personalrelease', False)),
-            'internal': 0,
-            'featured': 0,
-            'free': 0,
-            'doubleup': 0,
-            'sticky': 0,
-        }
-        # Internal
-        if self.config['TRACKERS'][self.tracker].get('internal', False) is True:
-            if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
-                data['internal'] = 1
-
-        if region_id != 0:
-            data['region_id'] = region_id
-        if distributor_id != 0:
-            data['distributor_id'] = distributor_id
-        if meta.get('category') == "TV":
-            data['season_number'] = meta.get('season_int', '0')
-            data['episode_number'] = meta.get('episode_int', '0')
-        headers = {
-            'User-Agent': f'Upload Assistant/2.2 ({platform.system()} {platform.release()})'
-        }
-        params = {
-            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
-        }
-
-        if meta['debug'] is False:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(
-                    url=self.upload_url,
-                    files=files,
-                    data=data,
-                    headers=headers,
-                    params=params,
-                    timeout=10
-                )
-            try:
-                meta['tracker_status'][self.tracker]['status_message'] = response.json()
-                # adding torrent link to comment of torrent file
-                t_id = response.json()['data'].split(".")[1].split("/")[3]
-                meta['tracker_status'][self.tracker]['torrent_id'] = t_id
-                await common.add_tracker_torrent(meta, self.tracker, self.source_flag, self.config['TRACKERS'][self.tracker].get('announce_url'), self.torrent_url + t_id)
-            except Exception:
-                meta['tracker_status'][self.tracker]['status_message'] = "It may have uploaded, go check"
-                return
-        else:
-            console.print("[cyan]Request Data:")
-            console.print(data)
-            meta['tracker_status'][self.tracker]['status_message'] = "Debug mode enabled, not uploading."
-
     async def edit_name(self, meta, cat_id):
         ldu_name = meta['name']
         non_eng = False
@@ -263,36 +140,3 @@ class LDU():
                 ldu_name = f"{ldu_name} {' '.join(language_parts)}"
 
         return ldu_name
-
-    async def search_existing(self, meta, disctype):
-        dupes = []
-        params = {
-            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
-            'tmdbId': meta['tmdb_id'],
-            'types[]': await self.get_type_id(meta['type'], meta),
-            'resolutions[]': await self.get_res_id(meta['resolution']),
-            'name': ""
-        }
-        if meta['category'] == 'TV':
-            params['name'] = params['name'] + f" {meta.get('season', '')}"
-        if meta.get('edition', "") != "":
-            params['name'] = params['name'] + f" {meta['edition']}"
-        try:
-            async with httpx.AsyncClient(timeout=10.0) as client:
-                response = await client.get(url=self.search_url, params=params)
-                if response.status_code == 200:
-                    data = response.json()
-                    for each in data['data']:
-                        result = [each][0]['attributes']['name']
-                        dupes.append(result)
-                else:
-                    console.print(f"[bold red]Failed to search torrents. HTTP Status: {response.status_code}")
-        except httpx.TimeoutException:
-            console.print("[bold red]Request timed out after 5 seconds")
-        except httpx.RequestError as e:
-            console.print(f"[bold red]Unable to search for existing torrents: {e}")
-        except Exception as e:
-            console.print(f"[bold red]Unexpected error: {e}")
-            await asyncio.sleep(5)
-
-        return dupes
