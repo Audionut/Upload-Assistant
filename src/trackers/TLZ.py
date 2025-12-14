aefb1a292bcf30b81f4e4fb7a437116e98939e8f
FortKnox1337 <34667229+FortKnox1337@users.noreply.github.com>
2025-12-14 00:45:10 +0000

    Add TLZ tracker file

---
+++ src/trackers/TLZ.py
@@ -0,0 +1,28 @@
+# Upload Assistant © 2025 Audionut — Licensed under UAPL v1.0
+# -*- coding: utf-8 -*-
+# import discord
+from src.trackers.COMMON import COMMON
+from src.trackers.UNIT3D import UNIT3D
+
+
+class TLZ(UNIT3D):
+    def __init__(self, config):
+        super().__init__(config, tracker_name='TLZ')
+        self.config = config
+        self.common = COMMON(config)
+        self.tracker = 'TLZ'
+        self.source_flag = 'TLZ'
+        self.base_url = 'https://tlzdigital.com'
+        self.id_url = f'{self.base_url}/api/torrents/'
+        self.upload_url = f'{self.base_url}/api/torrents/upload'
+        self.search_url = f'{self.base_url}/api/torrents/filter'
+        self.torrent_url = f'{self.base_url}/torrents/'
+        self.banned_groups = [""]
+        pass
+
+    async def get_category_id(self, meta):
+        category_id = {
+            'MOVIE': '1',
+            'TV': '2',
+        }.get(meta['category'], '0')
+        return {'category_id': category_id}
