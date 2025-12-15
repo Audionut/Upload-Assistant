4d0c322131c84548a293964c30ebe6db73c6945a
FortKnox1337 <34667229+FortKnox1337@users.noreply.github.com>
2025-12-15 00:01:27 +0000

    Add methods to get category and type IDs

--- src/trackers/TLZ.py
+++ src/trackers/TLZ.py
@@ -19,3 +19,28 @@ class TLZ(UNIT3D):
         self.torrent_url = f'{self.base_url}/torrents/'
         self.banned_groups = [""]
         pass
+        
+    async def get_category_id(self, meta):
+        category_id = {
+            'MOVIE': '1',
+            'TV': '2',
+        }.get(meta['category'], '0')
+        return {'category_id': category_id}
+
+    async def get_type_id(self, meta):
+        type_id = {
+            'FILM': '1',
+            'CAM': '2',
+            'EPISODE': '3',
+            'PACK': '4',
+        }.get(meta.get('type'), '0')
+        
+        if meta.get('tv_pack'):
+            type_id = '4'
+        elif type_id != '4':
+            type_id = '3'
+            
+        if meta['category'] == 'MOVIE':
+            type_id = '1'
+        
+        return {'type_id': type_id}
