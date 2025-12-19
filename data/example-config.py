b0e72873f1bf1618f476d39939fb1d9df94f8986
tetrahydroc <tetrahydroc@seedpool.org>
2025-12-18 23:36:27 -0500

    seedpool cdn support

--- data/example-config.py
+++ data/example-config.py
@@ -14,7 +14,7 @@ config = {
         "btn_api": "",
 
         # Order of image hosts. primary host as first with others as backup
-        # Available image hosts: imgbb, ptpimg, imgbox, pixhost, lensdump, ptscreens, onlyimage, dalexni, zipline, passtheimage
+        # Available image hosts: imgbb, ptpimg, imgbox, pixhost, lensdump, ptscreens, onlyimage, dalexni, zipline, passtheimage, seedpool_cdn
         "img_host_1": "",
         "img_host_2": "",
         "img_host_3": "",
@@ -32,6 +32,9 @@ config = {
         # custom zipline url
         "zipline_url": "",
         "zipline_api_key": "",
+        # Seedpool CDN settings
+        "seedpool_cdn_api": "",
+        "seedpool_cdn_url": "https://i.seedpool.org/upload",
 
         # Whether to add a logo for the show/movie from TMDB to the top of the description
         "add_logo": False,
