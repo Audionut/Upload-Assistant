c59bfcc29fd089463dc0b312aaedd0445e9809c3
FortKnox1337 <34667229+FortKnox1337@users.noreply.github.com>
2025-12-31 12:39:12 +0000

    YUS: Add further release groups to ban list
    Additional release groups have been added to the ban list. I'm not sure if this is a finalised list yet though.

--- src/trackers/YUS.py
+++ src/trackers/YUS.py
@@ -19,9 +19,11 @@ class YUS(UNIT3D):
         self.search_url = f'{self.base_url}/api/torrents/filter'
         self.torrent_url = f'{self.base_url}/torrents/'
         self.banned_groups = [
-            'KiNGDOM', 'Lama', 'MeGusta', 'MezRips', 'mHD', 'mRS', 'msd', 'NeXus', 'NhaNc3',
-            'nHD', 'PANDEMONiUM', 'RARBG', 'Radarr', 'RCDiVX', 'RDN', 'SANTi', 'VXT', 'Will1869', 'x0r',
-            'XS', 'YIFY', 'YTS', 'ZKBL', 'ZmN', 'ZMNT', 'D3US', 'B3LLUM', 'FGT', 'd3g']
+           ' ADDICTION', 'BANDOLEROS', 'BigEasy', 'B3LLUM', 'CINEMAXIS', 'DUMMESCHWEDEN', 'd3g', 'D3US', 'FGT', 'GRANiTEN',
+           'KiNGDOM', 'Lama', 'MeGusta', 'MezRips', 'mHD', 'mRS', 'msd', 'NeXus', 'NhaNc3','nHD', 
+           'NorTekst', 'NORViNE', 'PANDEMONiUM','PiTBULL', 'RAPiDCOWS',  'RARBG', 'Radarr', 'RCDiVX', 'RDN', 'ROCKETRACCOON',
+           'SANTi', 'SHOWTiME', 'SOOSi', 'SUXWIC', 'TWA', 'TOXVIO', 'VXT', 'Will1869', 'x0r','XS',
+           'YIFY', 'YOLAND', 'YTS', 'ZKBL', 'ZmN', 'ZMNT']
         pass
 
     async def get_additional_checks(self, meta):
