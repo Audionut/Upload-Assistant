77d347b6133353f79bbba70f0557ee3e2ad61754
wastaken7 <wellytonsn@gmail.com>
2026-01-23 00:56:59 -0300

    table

--- src/trackers/LUME.py
+++ src/trackers/LUME.py
@@ -7,14 +7,14 @@ from src.trackers.UNIT3D import UNIT3D
 
 class LUME(UNIT3D):
     def __init__(self, config: dict[str, Any]) -> None:
-        super().__init__(config, tracker_name='LUME')
+        super().__init__(config, tracker_name="LUME")
         self.config = config
         self.common = COMMON(config)
-        self.tracker = 'LUME'
-        self.base_url = 'https://luminarr.me'
-        self.id_url = f'{self.base_url}/api/torrents/'
-        self.upload_url = f'{self.base_url}/api/torrents/upload'
-        self.requests_url = f'{self.base_url}/api/requests/filter'
-        self.search_url = f'{self.base_url}/api/torrents/filter'
-        self.torrent_url = f'{self.base_url}/torrents/'
+        self.tracker = "LUME"
+        self.base_url = "https://luminarr.me"
+        self.id_url = f"{self.base_url}/api/torrents/"
+        self.upload_url = f"{self.base_url}/api/torrents/upload"
+        self.requests_url = f"{self.base_url}/api/requests/filter"
+        self.search_url = f"{self.base_url}/api/torrents/filter"
+        self.torrent_url = f"{self.base_url}/torrents/"
         self.banned_groups: list[str] = [""]
